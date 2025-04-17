9f94979b4c44b1f82718f218d7cd01f074d64aa1
kaushiksarangi26 <148024648+kaushiksarangi26@users.noreply.github.com>
2025-04-17 17:51:06 +0530

    Add files via upload

---
+++ src/main/java/com/telusko/SpringMVC/HomeController.java
@@ -0,0 +1,18 @@
+package com.telusko.SpringMVC;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+
+    @RequestMapping("/")
+    public String home(){
+        System.out.println("Inside home");
+        return "index";
+    }
+}
+
+
+
